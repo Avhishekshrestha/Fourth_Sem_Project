c6e8b8c4abf65e49aed7b381c679f97bebd3096b
Avhishek shrestha <sthaavhishek984@gmail.com>
2024-07-17 08:07:28 +0545

    third commit

--- app/src/main/java/com/example/fourth_sem_project/MainActivity.java
+++ app/src/main/java/com/example/fourth_sem_project/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.fourth_sem_project;
 
+import static com.example.fourth_sem_project.R.id.img;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    NavigationView nav;
+    // ActionBar actionbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        nav = findViewById(R.id.nav);
 
+        final DrawerLayout drawerLayout = findViewById(R.id.main);
+        findViewById(R.id.nav).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        ImageView imageView=findViewById(R.id.img);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
         //ActionBar actionBar=getSupportActionBar();
     }
 
